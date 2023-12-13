16eaa65a704331c95389285250d53fcb9e673b0e
Marco Valeri <marco.valeri@cherrythinking.com>
2023-12-13 13:40:34 +0000

    Delete block

--- blocks/testimonial/testimonial.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ACF value
-$testimonialTitle = get_field('testimonial_title');
-
-?>
-
-<div class="title">
-    <h2><?= $testimonialTitle; ?></h2>
-</div>
--- functions.php
+++ functions.php
@@ -11,10 +11,3 @@ function cherrytheme_enqueue_styles()
     wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', [], 1, true);
 }
 add_action('wp_enqueue_scripts', 'cherrytheme_enqueue_styles');
-
-// ACF
-function cherrytheme_register_acf_blocks()
-{
-    register_block_type(__DIR__ . '/blocks/testimonial');
-}
-add_action('init', 'cherrytheme_register_acf_blocks');
